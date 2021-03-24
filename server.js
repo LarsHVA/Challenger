3cc25df8e54ce8e87b54312d3c8c61560ba57464
deinca <zamoradhva@gmail.com>
2021-03-24 14:54:44 +0100

    mod server files

--- server.js
+++ server.js
@@ -121,9 +121,7 @@ express();
         'Client-ID': process.env.TWITCH_CLIENT_ID,
         'Authorization': process.env.TWITCH_APP_ACCESS_TOKEN,
     },
-    // data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
-    // data: "fields name, url, cover; where release_dates.platform = (48,49,6); limit 10;"
-    // data: 'fields id, name, cover; sort popularity desc; limit 5;where rating > 75;'
+
     data:'fields name, id; where rating > 70 & rating_count > 100 & aggregated_rating > 70 & aggregated_rating_count > 7  & release_dates.date > 1269387203; sort name asc; limit 100;' //1553384003
     
   })
